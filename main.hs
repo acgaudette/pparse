dd5e5bb7bbb246eba7489b15d9518202b7362862
Aaron C Gaudette <gaudetteaaron@gmail.com>
2018-06-15 18:15:57 -0400

    Comment

--- main.hs
+++ main.hs
@@ -132,7 +132,7 @@ scanNumber text =
   if char == ','
     then ("", remainder)
   else if char == ']'
-    then ("", text) -- Go straight back to ScanValue
+    then ("", text) -- Reuse char in scanValue
   else if char == ' ' -- Ignore spaces
     then scanNumber remainder
   else (char : fst result, snd result)
@@ -179,7 +179,7 @@ munge ignore =
     where char = head ignore
           remainder = tail ignore
 
--- No case to pascal case conversion
+-- No case to Pascal case conversion
 toPascal name = toUpper (head name) : toCamel (tail name)
 
 -- No case to camel case conversion
