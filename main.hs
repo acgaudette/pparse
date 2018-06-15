3b2c586805e383254ee6ea4a377d969de5b45bda
Aaron C Gaudette <gaudetteaaron@gmail.com>
2018-06-15 19:00:49 -0400

    Move command-line options code to separate module

--- main.hs
+++ main.hs
@@ -1,67 +1,8 @@
 import System.IO
 import System.Environment
-import System.Console.GetOpt
 import Data.Char
 
-data Options = Options {
-    optInput :: String
-  , optOutput :: String
-  , optNamespace :: Maybe String
-  , optContainer :: Maybe String
-  , optIncludes :: [String]
-  , optIgnores :: [String]
-  , optCherries :: [String]
-}
-
-defaultOptions = Options {
-    optInput = "in.js"
-  , optOutput = "out.cs"
-  , optNamespace = Nothing
-  , optContainer = Nothing
-  , optIncludes = []
-  , optIgnores = []
-  , optCherries = []
-}
-
-options = [
-    Option ['i'] ["input"] (
-      ReqArg (\ path options -> options { optInput = path })
-      "path"
-    ) "file for reading"
-  , Option ['o'] ["output"] (
-      ReqArg (\ path options -> options { optOutput = path })
-      "path"
-    ) "file for writing"
-  , Option ['n'] ["namespace"] (
-      ReqArg (\ name options -> options { optNamespace = Just name })
-      "namespace"
-    ) "namespace for output header"
-  , Option ['c'] ["container"] (
-      ReqArg (\ name options -> options { optContainer = Just name })
-      "container"
-    ) "container for output class"
-  , Option ['l'] ["include"] (
-      ReqArg (\ include options ->
-        options { optIncludes = optIncludes options ++ [include] })
-      "include"
-    ) "using directive for output header"
-  , Option ['I'] ["ignore"] (
-      ReqArg (\ name options ->
-        options { optIgnores = optIgnores options ++ [name] })
-      "name"
-    ) "property name to ignore"
-  , Option ['C'] ["cherry"] (
-      ReqArg (\ name options ->
-        options { optCherries = optCherries options ++ [name] })
-      "name"
-    ) "container name to parse"
-  ]
-
-getOptions args =
-  case getOpt Permute options args of
-    (o, _, []) -> return (foldl (flip id) defaultOptions o)
-    (_, _, e ) -> ioError (userError (concat e ++ usageInfo message options))
-  where message = "Usage: pparse [option...]"
+import Options
 
 main = do
   args <- getArgs
