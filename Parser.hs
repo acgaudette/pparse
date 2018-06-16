450b1d3cd2029bb74f916856416fb86569eeebe3
Aaron C Gaudette <gaudetteaaron@gmail.com>
2018-06-15 21:01:44 -0400

    Generate accessor before field

--- Parser.hs
+++ Parser.hs
@@ -100,9 +100,11 @@ genFields tokens ignores =
 genField name next ignores =
   if elem name (map munge ignores) then "" -- Skip ignored names
   else case next of
-    Value _ min max -> mkRange min max ++ field
+    Value _ min max -> accessor ++ mkRange min max ++ field
     _ -> tab 3 ++ field
-  where field = mkFloat ++ toCamel name
+  where camel = toCamel name
+        accessor = mkAccessor (toPascal name) camel name
+        field = mkFloat ++ camel
 
 -- Replace underscore with space
 munge ignore =
